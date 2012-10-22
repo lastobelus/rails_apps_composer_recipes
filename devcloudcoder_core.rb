d572a9cb72e45882caa7b6d6b8c35ee80ec8aa52
Michael Johnston <lastobelus@mac.com>
2012-10-21 19:08:12 -0700

    fix to devcloudcoder recipe

--- devcloudcoder_core.rb
+++ devcloudcoder_core.rb
@@ -1,1 +1,12 @@
-gem "config_spartan",  git: "git://github.com/lastobelus/rails_apps_composer.git", branch: "devcloudcoder"
+gem "rails_apps_composer",  git: "git://github.com/lastobelus/rails_apps_composer.git", branch: "devcloudcoder"
+
+__END__
+
+name: devcloudcoder_core
+description: "Core setup for a devcloudcoder app"
+author: lastobelus
+
+requires: [setup, gems]
+run_after: [setup, gems]
+category: configuration
+
