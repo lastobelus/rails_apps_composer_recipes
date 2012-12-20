d7eab5f22602a6ad1a2fb0c12bcbbc9acc8fd6e6
Michael Johnston <lastobelus@mac.com>
2012-12-20 12:37:50 -0800

    add git to css_canon recipe

--- css_canon.rb
+++ css_canon.rb
@@ -3,6 +3,9 @@ gem 'css_canon'
 after_bundler do
   say "setting up 'canonical' css structure"
   generate "active_admin:install #{config['activeadmin_admin_user_model']}#{registerable}"  
+
+  git :add => '-A' if prefer :git, true
+  git :commit => '-qm "devcloudcoder: css_canon"' if prefer :git, true
 end
 
 
@@ -12,7 +15,6 @@ name: CssCanon
 description: "Set up a 'canonical' css structure"
 author: lastobelus
 
-category: other
+category: frontend
 
-config:
- 
+config: 
