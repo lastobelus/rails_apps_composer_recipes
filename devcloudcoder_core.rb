2f81579406c1948258ed5fadc3f269ad92bdb1b3
Michael Johnston <lastobelus@mac.com>
2013-04-21 18:29:56 -0700

    use splat for gemspec args

--- devcloudcoder_core.rb
+++ devcloudcoder_core.rb
@@ -1,7 +1,10 @@
+require 'devcloudcoder'
 gem "rails_apps_composer",  git: "git://github.com/lastobelus/rails_apps_composer.git", branch: "devcloudcoder"
+
 gem "grit", group: "development"
 # TODO: this is a nasty lurking dependency and all all these recipes need to be factored back in to devcloudcoder, removing their rails_apps_composer nature and making them regular rails generators OR thor tasks
-gem ::Devcloudcoder.config.devcloudcoder_gem_spec_for_gemfile
+
+gem(*::Devcloudcoder.config.devcloudcoder_gem_spec_for_gemfile)
 
 __END__
 
